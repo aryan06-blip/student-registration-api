053ccc5c13cba7966d2fe68d4263c16bb254cfbc
aryan06-blip <aryan.2428cseaiml273@kiet.edu>
2026-02-24 14:31:22 +0530

    Add files via upload

---
+++ routes/studentRoutes.js
@@ -0,0 +1,84 @@
+const express = require("express");
+const router = express.Router();
+const Student = require("../models/student");
+
+
+// Add new student
+router.post("/", async (req, res) => {
+    try {
+        const student = new Student(req.body);
+        await student.save();
+        res.status(201).json(student);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+});
+
+
+// View all students
+router.get("/", async (req, res) => {
+    try {
+        const students = await Student.find();
+        res.json(students);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+
+// View student by ID
+router.get("/:id", async (req, res) => {
+    try {
+        const student = await Student.findById(req.params.id);
+
+        if (!student)
+            return res.status(404).json({ message: "Student not found" });
+
+        res.json(student);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+
+// Update student
+router.put("/:id", async (req, res) => {
+    try {
+        const student = await Student.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            { new: true }
+        );
+
+        res.json(student);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+});
+
+
+// Delete student
+router.delete("/:id", async (req, res) => {
+    try {
+        await Student.findByIdAndDelete(req.params.id);
+        res.json({ message: "Student deleted successfully" });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+
+// Search students by course
+router.get("/search/course/:course", async (req, res) => {
+    try {
+        const students = await Student.find({
+            course: req.params.course
+        });
+
+        res.json(students);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+module.exports = router;
